98cbdb717558acff00b231671fbdc0a2b2fcc978
january89 <eightynine01@naver.com>
2016-05-03 12:12:33 +0900

    리팩토링 하였으나 여전히 느림..

--- src/main/java/effective/MirrorNumber.java
+++ src/main/java/effective/MirrorNumber.java
@@ -6,19 +6,27 @@ public class MirrorNumber{
 
     public static void main(String[] args) {
 
-        long i = 11l;
+        int i = 11;
 
         while(true){
 
-            if( String.valueOf(i).equals((new StringBuffer(String.valueOf(i)).reverse()).toString())
-                    && Long.toBinaryString(i).equals((new StringBuffer(Long.toBinaryString(i)).reverse()).toString())
-                    && Long.toOctalString(i).equals((new StringBuffer(Long.toOctalString(i)).reverse()).toString()) ) break;
+            if( String.valueOf(i).equals(ReverseInteger(i))
+                    && Integer.toBinaryString(i).equals(ReverseInteger(Integer.toBinaryString(i)))
+                    && Integer.toOctalString(i).equals(ReverseInteger(Integer.toOctalString(i))) ) break;
 
             i += 2;
+
         }
 
         out.println(i);
 
+    }
+
+    private static String ReverseInteger(int i){
+        return new StringBuffer(String.valueOf(i)).reverse().toString();
+    }
+    private static String ReverseInteger(String i){
+        return new StringBuffer(i).reverse().toString();
     }
 
 }

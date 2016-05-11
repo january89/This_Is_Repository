f500b1c1b8c6b34d2afac29a12e5d71491e2addf
january89 <eightynine01@naver.com>
2016-05-11 21:08:55 +0900

    Netty 88p 까지 완료!
    EchoServerV2
    EchoServerV3
    
    두가지 예제파일 추가!

---
+++ src/main/java/server/echo/EchoServerV3.java
@@ -0,0 +1,38 @@
+package server.echo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * Created by Park Tae Hwan on 2016-05-11.
+ */
+public class EchoServerV3{
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup worketGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup,worketGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch){
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new LoggingHandler(LogLevel.DEBUG));
+                            p.addLast(new EchoServerHandler());
+                        }
+                    });
+        }
+        finally {
+            worketGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
